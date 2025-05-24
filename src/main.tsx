38d3fd324b7f7be91fd69ed18fa3c4f05ab38992
Anggi Fergian <a.fergian.pratama@accenture.com>
2025-05-24 07:06:32 +0700

    feat: init first blog page

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import Layout from './components/Layout.tsx'
 import Profile from './pages/Profile.tsx'
 import Home from './pages/Home.tsx'
 import Project from './pages/projects/project-list.tsx'
+import BlogLayout from './components/common/blog-layout.tsx'
+import InstallNginxVps from './pages/blogs/install-nginx-vps/install-nginx-vps.tsx'
 
 const router = createBrowserRouter([
   {
@@ -31,6 +33,16 @@ const router = createBrowserRouter([
       }
     ],
   },
+  {
+    path: '/blog',
+    element: <BlogLayout />,
+    children: [
+      {
+        path: 'install-nginx-vps',
+        element: <InstallNginxVps />
+      }
+    ]
+  }
 ]);
 
 createRoot(document.getElementById('root')!).render(
---
+++ src/pages/blogs/install-nginx-vps/install-nginx-vps.tsx
@@ -0,0 +1,5 @@
+import InstallNginxVPS from './install-nginx-vps.mdx';
+
+export default function InstallNginxVps() {
+    return <InstallNginxVPS />
+}
