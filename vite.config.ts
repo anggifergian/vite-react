465c0e283022969a35c227a2949cc18e0b5c4513
Anggi Fergian <a.fergian.pratama@accenture.com>
2025-05-24 07:05:49 +0700

    deps: @mdx-js/react, @mdx-js/rollup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import mdx from '@mdx-js/rollup'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), mdx({providerImportSource: '@mdx-js/react',})],
   server: {
     port: 3000,
     strictPort: true,
