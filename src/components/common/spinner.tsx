cdb60850809b74fd70892dbaaef51b1f67afd848
Anggi Fergian <anggyfergian56@gmail.com>
2025-05-24 12:09:40 +0700

    feat: add new component

---
+++ src/components/common/spinner.tsx
@@ -0,0 +1,9 @@
+const Spinner = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-t-transparent border-blue-500" />
+    </div>
+  );
+};
+
+export default Spinner;
